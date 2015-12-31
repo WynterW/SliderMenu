a7be96bdb8430be6b54a45b3700d76b9d4136d64
Wynter <564640283@qq.com>
2015-12-31 09:54:45 +0800

    Initial Commit

---
+++ SliderMenu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SliderMenu
+//
+//  Created by Wynter on 15/12/31.
+//  Copyright © 2015年 Wynter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
